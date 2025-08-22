9721ebff55d20a41dbb4221269956d1cef29f4ba
Muhammad Zeeshan <zeeshahmed7890@gmail.com>
2025-08-22 05:39:29 -0400

    spatie

---
+++ app/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+
+    use HasFactory,  HasRoles, Notifiable;
+
+    
+    protected $guard_name = 'api';
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+
+    public function roles()
+    {
+        return $this->morphToMany(Role::class, 'model', 'model_has_roles', 'model_id', 'role_id');
+    }
+
+    public function permissions()
+    {
+        return $this->MorphToMany(Permission::class, 'model', 'model_has_permission', 'model_id', 'permission_id');
+    }
+}
